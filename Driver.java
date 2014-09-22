0ac4eb37602e6fb667194f18f0be458223d2ed63
Melisssssssssa <melissa.ye97@gmail.com>
2014-09-21 23:30:00 -0400

    updated hw3

--- Driver.java
+++ Driver.java
@@ -2,7 +2,15 @@ public class Driver{
     public static void main(String[] args){
 	String s = "melissa ye";
 	String p = String.indexOf("e");
+	String start = s.indexOF("m");
+	String end = s.indexOF(" ");
+	String last = s.indexOF("ye");
 
+
+	public String substring( start, end);
+	public String substring( end, last);
+
+	
 
 
     }
