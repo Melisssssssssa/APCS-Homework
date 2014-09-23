3d459b3bc883cef5ee63316b2ad91297f4a7252d
Melisssssssssa <melissa.ye97@gmail.com>
2014-09-23 19:39:36 -0400

    Homework 5

---
+++ 05-Codingbat/Driver (2).java
@@ -0,0 +1,17 @@
+public class Driver{
+    public String nonStart(String s, String p){
+	return s.substring(1) +b.substring(1);
+    }
+    public int diff21(int n){
+	if (n > 21){
+	    return 2 * Math.abs(21 - n);
+	}
+	else {
+	    return Math.abs(21 - n);
+	}
+    }
+
+    public String makeAbba(String s, String p){
+	return s + p + p + s;
+    }
+}
