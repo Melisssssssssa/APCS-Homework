ed7aef63dc0674acb1f7c80d1a56be87f530e2da
Melisssssssssa <melissa.ye97@gmail.com>
2014-09-23 20:07:48 -0400

    Homework #4

---
+++ 04-Stringstuff/Driver.java
@@ -0,0 +1,21 @@
+public class driver{
+    public static void main(String[] args){
+	public boolean mixStart(String str); {
+	    if (Str.length() >3){
+		return str.substring(1,3).equals("xi");
+	    }
+	    else {
+		reutrn false;
+	    }
+	}
+
+	public String makeOutWord(String out, String word) {
+	    return out.substring(0,2) +word+out.substring(2,4);
+	}
+
+	public String firstHalf(String str){
+	    return str.substring(0,(str.length()/2));
+	}
+    }
+}
+
