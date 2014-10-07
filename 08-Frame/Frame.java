dd38f256aed167ca6bc0bf27cdd6fa63cf96949c
Melisssssssssa <melissa.ye97@gmail.com>
2014-10-06 22:40:09 -0400

    Homwork

--- 08-Frame/Frame.java
+++ 08-Frame/Frame.java
@@ -1,2 +1,10 @@
 public String Frame( int r, int c) {
     
+    String s = "";
+    s += line(c,'*') + "\n";
+    for(int i = 0;i < r - 2; i ++){
+    s += '*' + line(c - 2,' ') + "*\n";
+	}
+    s += line(c, '*');
+    return s;
+}
