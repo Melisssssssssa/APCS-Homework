0ef51830787026f7395d01ed4f8b0ec86b5a01e2
Melisssssssssa <melissa.ye97@gmail.com>
2014-11-20 22:28:12 -0500

    M

--- 18-Wordsearch/WordSearch.java
+++ 18-Wordsearch/WordSearch.java
@@ -1,130 +1,117 @@
-public class WordSearch{
-
+public class WordSearch {
     private char[][] board;
 
-    public WordSearch (int r, int c){
-
+    public WordSearch(int r, int c){
 	board = new char[r][c];
-	for ( int i = 0; i < board.length; i++){
-	    for (int j =0;j< board[i].length; j++){
-		board [i][j]=".";
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[i].length; j++) {
+		board[i][j]='.';
 	    }
 	}
-	
     }
-
-    public WordSearch{
-	this(30,20);
+    public WordSearch() {
+	this(20,40);
     }
-
-    
-
+ 
     public String toString(){
-	String s = " ";
-	for (int i =0;i< board.length; i++){
-	    for (int j=0; j< board[i].length;j++){
-		s =s +board[i][j];
+	String s = "";
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[i].length; j++) {
+		s = s + board[i][j];
 	    }
-	    s= s + "\n";
+	    s = s + "\n";
 	}
 	return s;
     }
-
-    public boolean canAdd(String w, int row, int col) {
-	int r = row, c = col;
-	boolean b;
-	for (int i=0; i<w.length(); i++) {
-	    if (board[r][c] !== ".") {
-		b = true;
-	    }
+    
+    public boolean checkIndex(int i, int RorC){
+	boolean OK = false;
+	if (RorC == 0){
+	    if (i < board.length && i > 0)
+		OK = true;
+	} else if (RorC == 1){
+	    if (i < board[0].length && i > 0)
+		OK = true;
 	}
-	return b;
+	if (!OK) System.out.println("Index out of Range. addWord canceled.");
+	return OK;
     }
-  public void addWordH(String w, int row, int col) {
-	int r = row, c = col;
-	for (int i=0; i<w.length();i++) {
-	    if (canAdd(w, r, c)) {
-		board[r][c] = w.charAt(i);
-		c++;
-	    }
-	    if(c>board[r].length) {
+    
+    public boolean checkOverH(int row, int col, String w){
+	boolean OK = true;
+	
+	for (int a = 0; a < w.length(); a++){
+	    if (board[row][a + col] != w.charAt(a) && board[row][a + col] != '.') {
+		OK = false;
 		break;
 	    }
 	}
-    }
-
-    public void addWordHback(String w, int row, int col) {
-	int r = row, c = col;
-
-	for(int i=s.length()-1;i>=0;i--) {
-	    if (canAdd(w, r, c)) {
-		board[r][c] = s.charAt(i);
-		c++;
-	    }
-	    if(c<0) { 
-		break; 
-	    }
-	}
-    }
-    
-    public void addWordV(String w, int row, int col) {
-	int r = row, c = col;
-	for (int i=0; i<w.length();i++) {
-	    board[c][r] = w.charAt(i);
-	    c++;
-	}
-    }
-    
-    public void addWordSE(String w, int row, int col) {
-	int r = row, c = col;
-	for (int i=0; i<w.length();i++) {
-	    board[r][c] = w.charAt(i);
-	    c++;
-	    r++;
-	}
-    }
-
-    public void addWordNE(String w, int row, int col) {
-	int r = row, c = col;
-	for (int i=0; i<w.length();i++) {
-	    board[r][c] = w.charAt(i);
-	    c++;
-	    r--;
-	}
-    }
-
-    public void addWordNW(String w, int row, int col) {
-	int r = row, c = col;
-	for (int i=0; i<w.length();i++) {
-	    board[r][c] = w.charAt(i);
-	    c--;
-	    r--;
-	}
-    }
-
-    public void addWordSW(String w, int row, int col) {
-	int r = row, c = col;
-	for (int i=0; i<w.length();i++) {
-	    board[r][c] = w.charAt(i);
-	    c--;
-	    r++;
-	}
-    }
-
-    public static void main(String[] args){
-	WordSearch w = new WordSearch();
-	System.out.println(w);
-	w.addWordH("hello",3,5);
-	w.addWordV("look",4,8);
-	w.addWordSE("nihao",6,9);
-	w.addWordNE("konichiwa",11,13);
-	w.addWordNW("ciao",17,15);
-	w.addWordSW("hola",14,8);
-	System.out.println(w);
-
-
-    }
-}
-
 	
+	if (!OK) System.out.println("Word already there & not overlapping. addWord canceled.");
+	return OK;
+    }
+    
+    public boolean checkOverV(int row, int col, String w){
+	boolean OK = true;
+	for (int a = 0; a < w.length(); a++){
+	    if (board[row + a][col] != w.charAt(a) && board[row + a][col] != '.'){
+		OK = false;
+		break;
+	    }
+	}
+	if (!OK) System.out.println("Word already there & not overlapping. addWord canceled.");
+	return OK;
+    }
+    
+    // direc is either 1 or -1 (forwards or backwards)
+    
+    public String reverseString(String s){
+	String t = "";
+	for (int j = 0; j < s.length(); j++){
+	    t = s.charAt(j) + t;
+	}
+	return t;
+    }
+    
+    public void addWordH(String w, int row, int col,int direc){
+	if (direc == -1) w = reverseString(w);
+	if (checkIndex(col + w.length() - 1,1) && checkIndex(row,0)){
+	    if (checkOverH(row,col,w)){
+		int c = col;
+		for (int loc = 0; loc < w.length(); loc++){
+		    board[row][c] = w.charAt(loc);
+		    c++;
+		}
+	    }
+	}
+    }
+    
+    public void addWordV(String w, int row, int col, int direc){
+	if (direc == -1) w = reverseString(w);
+	if (checkIndex(col, 1) && checkIndex(row + w.length() - 1,0)){
+	    if (checkOverV(row,col,w)){
+		int r = row;
+		for (int loc = 0; loc < w.length(); loc++){
+		    board[r][col] = w.charAt(loc);
+		    r++;
+		}
+	    }
+	}
+    }
+    public static void main(String[] args) {
+	WordSearch w = new WordSearch(10,40);
+	//System.out.println(w);
+	w.addWordH("look",3,14,1); // test illegal overlap
+	w.addWordH("look",3,17,1); // test legal overlap
+	w.addWordH("look",-3,20,1); // test illegal row
+	w.addWordH("look",3,55,1); // test illegal col
+	w.addWordH("olleh",4,14,-1);
+       	w.addWordH("hello",4,18,-1);
+	w.addWordH("olleh",4,22,-1);
+	w.addWordH("dogs",6,3,1);
+	w.addWordV("hello",2,4,1);
+	w.addWordV("spmal",4,24,-1);
+	System.out.print(w);
+    }
+
 }
