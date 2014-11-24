6ddd6af6202bdcf4e676bfc0856385a4085f0caf
Melisssssssssa <melissa.ye97@gmail.com>
2014-11-23 22:10:10 -0500

    m

---
+++ 18-Wordsearch/Driver.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.io.File;
+
+public class Driver{
+    public static void main(String[] args) {
+	WordSearch w = new WordSearch();
+	Scanner sc = null;
+	try {
+	    sc =new Scanner(new File("words.txt"));
+	}	catch (Exception e) {
+	    System.out.println("File not found");
+	    System.exit(0);
+	}
+	while (sc.hasNext()) {
+	    String s = sc.next();
+	    w.addWord(s);	
+	}
+	System.out.println(w);
+	System.out.println("good luck LOL\n");
+	w.fillBoard();
+	System.out.print(w);
+    }
+}
