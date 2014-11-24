1f4e5cd94d64eccbd727015db36d009b0d391086
Melisssssssssa <melissa.ye97@gmail.com>
2014-11-23 22:07:13 -0500

    m

--- 18-Wordsearch/WordSearch.java
+++ 18-Wordsearch/WordSearch.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class WordSearch {
     private char[][] board;
 
@@ -24,94 +26,75 @@ public class WordSearch {
 	return s;
     }
     
-    public boolean checkIndex(int i, int RorC){
-	boolean OK = false;
-	if (RorC == 0){
-	    if (i < board.length && i > 0)
-		OK = true;
-	} else if (RorC == 1){
-	    if (i < board[0].length && i > 0)
-		OK = true;
-	}
-	if (!OK) System.out.println("Index out of Range. addWord canceled.");
-	return OK;
+    public boolean checkIndex(String w, int row, int col, int upOrDown, int rightOrLeft){
+	boolean Ok = true;
+	int len = w.length() - 1;
+	if (upOrDown == 1 && Ok) Ok = (row + len < board.length);
+	if (upOrDown == 0 && Ok) Ok = (row < board.length);
+	if (upOrDown == -1 && Ok) Ok = (row - len > 0);
+	if (rightOrLeft == 1 && Ok) Ok = (col + len < board[0].length);
+	if (rightOrLeft == 0 && Ok) Ok = (col < board[0].length);
+	if (rightOrLeft == -1 && Ok) Ok = (col - len > 0);
+	Ok = (row > -1 && col > -1 && row < board.length && col < board[0].length && Ok);
+	return Ok;
     }
     
-    public boolean checkOverH(int row, int col, String w){
+    public boolean checkOver(String w, int row, int col, int upOrDown, int rightOrLeft){
 	boolean OK = true;
-	
 	for (int a = 0; a < w.length(); a++){
-	    if (board[row][a + col] != w.charAt(a) && board[row][a + col] != '.') {
+	    if (board[row + (upOrDown * a)][col + (rightOrLeft * a)] != w.charAt(a) && board[row + (upOrDown * a)][col + (rightOrLeft * a)] != '.') {
 		OK = false;
 		break;
 	    }
 	}
-	
-	if (!OK) System.out.println("Word already there & not overlapping. addWord canceled.");
 	return OK;
     }
     
-    public boolean checkOverV(int row, int col, String w){
-	boolean OK = true;
-	for (int a = 0; a < w.length(); a++){
-	    if (board[row + a][col] != w.charAt(a) && board[row + a][col] != '.'){
-		OK = false;
-		break;
-	    }
-	}
-	if (!OK) System.out.println("Word already there & not overlapping. addWord canceled.");
-	return OK;
-    }
-    
-    // direc is either 1 or -1 (forwards or backwards)
-    
-    public String reverseString(String s){
-	String t = "";
-	for (int j = 0; j < s.length(); j++){
-	    t = s.charAt(j) + t;
-	}
-	return t;
-    }
-    
-    public void addWordH(String w, int row, int col,int direc){
-	if (direc == -1) w = reverseString(w);
-	if (checkIndex(col + w.length() - 1,1) && checkIndex(row,0)){
-	    if (checkOverH(row,col,w)){
+    public boolean addWordA(String w, int row, int col, int upOrDown, int rightOrLeft){
+	if (checkIndex(w, row, col, upOrDown, rightOrLeft)){
+	    if (checkOver(w, row, col, upOrDown, rightOrLeft)){
 		int c = col;
-		for (int loc = 0; loc < w.length(); loc++){
-		    board[row][c] = w.charAt(loc);
-		    c++;
+		int r = row;
+		for (int loc = 0; loc < w.length(); loc++){    
+		    board[r][c] = w.charAt(loc);
+		    r += upOrDown;
+		    c += rightOrLeft;
 		}
 	    }
+	    return true;
+	} 
+	return false;
+    }
+		
+    public boolean addWord(String w){
+	Random r = new Random();
+	boolean in = false;
+	for (int tries = 0; tries < 100; tries++){
+	    int row = r.nextInt(board.length);
+	    int col = r.nextInt(board[0].length);
+	    int upOrDown = r.nextInt(3) - 1;
+	    int rightOrLeft = r.nextInt(3) - 1;
+	    while (upOrDown == 0 && rightOrLeft == 0){
+		upOrDown = r.nextInt(3) - 1;
+		rightOrLeft = r.nextInt(3) - 1;
+	    }
+	    if (addWordA(w, row, col, upOrDown, rightOrLeft)){
+		in = true;
+		break;
+	    }
 	}
+	return in;
     }
     
-    public void addWordV(String w, int row, int col, int direc){
-	if (direc == -1) w = reverseString(w);
-	if (checkIndex(col, 1) && checkIndex(row + w.length() - 1,0)){
-	    if (checkOverV(row,col,w)){
-		int r = row;
-		for (int loc = 0; loc < w.length(); loc++){
-		    board[r][col] = w.charAt(loc);
-		    r++;
+    public void fillBoard(){
+	String alphabet = "qwertyuiopasdfghjklzxcvbnm";
+	Random location = new Random();
+	for (int b = 0; b < board.length; b++){
+	    for (int d = 0; d < board[b].length; d++){
+		if (board[b][d] == '.'){
+		    board[b][d] = alphabet.charAt(location.nextInt(26));
 		}
 	    }
 	}
     }
-    public static void main(String[] args) {
-	WordSearch w = new WordSearch(10,40);
-	//System.out.println(w);
-	w.addWordH("look",3,14,1); // test illegal overlap
-	w.addWordH("look",3,17,1); // test legal overlap
-	w.addWordH("look",-3,20,1); // test illegal row
-	w.addWordH("look",3,55,1); // test illegal col
-	w.addWordH("olleh",4,14,-1);
-       	w.addWordH("hello",4,18,-1);
-	w.addWordH("olleh",4,22,-1);
-	w.addWordH("dogs",6,3,1);
-	w.addWordV("hello",2,4,1);
-	w.addWordV("spmal",4,24,-1);
-	System.out.print(w);
-    }
-
 }
